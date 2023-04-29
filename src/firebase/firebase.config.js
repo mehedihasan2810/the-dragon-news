a693bafe56a56789dd1136bf090bdffcf6ab112b
mehedi-hasan <mdmehedihasan2810@gmail.com>
2023-04-29 17:25:20 +0600

    change the rating component

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-console.log(import.meta.env.VITE_APIKEY);
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- src/pages/Home/NewsCard/NewsCard.jsx
+++ src/pages/Home/NewsCard/NewsCard.jsx
@@ -3,9 +3,10 @@ import { Card, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { FaEye, FaRegBookmark } from "react-icons/fa";
 import { FaShareAlt } from "react-icons/fa";
-import { FaStar } from "react-icons/fa";
-import { FaRegStar } from "react-icons/fa";
-import Rating from "react-rating";
+
+import { Rating } from "@smastrom/react-rating";
+
+import "@smastrom/react-rating/style.css";
 
 const NewsCard = (news) => {
   const { _id, title, details, image_url, total_view } = news;
@@ -35,14 +36,8 @@ const NewsCard = (news) => {
         </Card.Text>
       </Card.Body>
       <Card.Footer className="text-muted d-flex">
-        <div className="flex-grow-1">
-          <Rating
-            placeholderRating={number}
-            readonly
-            emptySymbol={<FaRegStar />}
-            placeholderSymbol={<FaStar className="text-warning" />}
-            fullSymbol={<FaStar />}
-          />
+        <div className="flex-grow-1 d-flex align-items-center">
+          <Rating style={{ maxWidth: 150 }} value={3} readOnly />
           <span className="ms-2">{number}</span>
         </div>
         <div>
